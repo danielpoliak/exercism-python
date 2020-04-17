6a3c9826420a7d115720a510151ceead9e43daab
Daniel Poliak <daniel.poliak@erstegroup.com>
2020-04-17 21:56:11 +0200

    added two fer

---
+++ two-fer/two_fer.py
@@ -0,0 +1,10 @@
+def two_fer(name={}):
+    if name is two_fer.__defaults__[0]:
+        sentence = "One for you, one for me."
+        return sentence
+    else:
+        sentence = "One for {name}, one for me.".format(name = name)
+        return sentence
+
+
+
